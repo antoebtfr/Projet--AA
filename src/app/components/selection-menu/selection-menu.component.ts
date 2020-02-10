75f8e34dd456fd3ab2666adc840eb1f23917d084
cdhbze93 <antoebt@hotmail.com>
2020-02-10 19:32:14 +0100

    feat: try to filter

--- src/app/components/selection-menu/selection-menu.component.ts
+++ src/app/components/selection-menu/selection-menu.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectionMenuComponent implements OnInit {
 
+  public modesArray = [
+    {name : 'Mode 1', TriggerContent :  '', img : 'https://i.pinimg.com/originals/f0/96/05/f096057b84da97d0ab740b5fb4508838.jpg'},
+    {name : 'Mode 2', TriggerContent :  '', img : ''},
+    {name : 'Mode 3', TriggerContent :  '', img : ''},
+  ];
   constructor() { }
 
   ngOnInit() {
